f87a184c3a9596e7f1620c6f2641dd7e4bd599a9
eshoiza <engrjayt200@gmail.com>
2020-02-26 13:47:23 +0100

    added a test file to perform the test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import {configure,mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import App from './App';
+import * as API from './Api';
+import { resolve } from 'dns';
+import expectExport from 'expect';
+
+configure({adapter: new Adapter()});
+
+describe('<App>', ()=>{
+  const randomImagePath =
+    "https://en.wikipedia.org/wiki/Devil_May_Cry_3:_Dante%27s_Awakening#/media/File:Devil_May_Cry_3_boxshot.jpg";
+
+  const dummyPokemon = {
+    sprites:{
+      front_default: randomImagePath
+    }
+  };
+  const getPokemonSpy = jest.spyOn(API, 'getPokmon').mockImplementation(()=>new Promise(resolve=>resolve(dummyPokemon)));
+  it('should call getPokemon after entering a pokemon name',()=>{
+    const wrapper  = mount(<App />);
+    wrapper.find('input').simulate('change', {target:{value: 'pikachu'}});
+    wrapper.find('button').simulate('click');
+    expect(getPokemonSpy).toHaveBeenCallWidth('pickachu')
+  });
+
+})
